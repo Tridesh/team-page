76e2b85ad96652bb7faeb261e70b295b61a349a6
Tridesh <tridesh2005@gmail.com>
2024-05-19 13:58:35 +0530

    Changed design

--- pages/page.jsx
+++ pages/page.jsx
@@ -12,8 +12,16 @@ const Home = () => {
                     <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
                     <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
                     <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
+                    <img className = "card" src = "./profileCard2.png" alt="profileCard"></img>
                 </div>
             </div>
+            <div className="bottom-border"></div>
             
         </div>
     );
