e447ab9dbef2da1e906f82d4b28e851ff33ea4f1
Tridesh <tridesh2005@gmail.com>
2024-05-19 10:47:14 +0530

    profile card

--- pages/page.jsx
+++ pages/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./style.css";
 
 const Home = () => {
     return (
         <div>
             This the team page
+            <img className="pic" src = "./profileCard.png" alt = "profile card" width={300}></img>
         </div>
     );
 };
