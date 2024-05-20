7966090562da8c0b011b817792480f9e39e479d0
Tridesh <tridesh2005@gmail.com>
2024-05-20 10:13:07 +0530

    Design update

--- pages/page.jsx
+++ pages/page.jsx
@@ -31,7 +31,7 @@ const Home = () => {
         <div>
             <div className="row-1">
             <div className="text1">
-                <p className="header">Web Team 😎</p> 
+                <p className="header">Web Team</p> 
                 <p className="desc">Step into the realm where pixels dance and algorithms sing—the Tech Team of our club. A sanctuary for the curious minds, the inventive souls, and the digital dreamers. Here, we craft the future, line by line, pixel by pixel. </p>
             </div>
             <div className="scrollin">
@@ -40,14 +40,14 @@ const Home = () => {
             </div>
             <div className="scrollBar1" ref={scroll_sectionRef}>
                 <div className="profileCard">
+                    {/* <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
-                    <img className="card" src="./profileCard2.png" alt="profile cards"></img>
-                    <img className="card" src="./profileCard2.png" alt="profile cards"></img>
-                    {/* {[...Array(10)].map((_, index) => (
+                    <img className="card" src="./profileCard2.png" alt="profile cards"></img> */}
+                    {[...Array(10)].map((_, index) => (
                         <img key={index} className="card" src="./profileCard2.png" alt="profileCard" />
-                    ))} */}
+                    ))}
                 </div>
             </div>
             
@@ -60,14 +60,14 @@ const Home = () => {
             </div>
             <div className="scrollBar1 left" ref={scroll_sectionRef}>
                 <div className="profileCard">
-                    {/* {[...Array(10)].map((_, index) => (
+                    {[...Array(10)].map((_, index) => (
                         <img key={index} className="card" src="./profileCard2.png" alt="profileCard" />
-                    ))} */}
+                    ))}
+                    {/* <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
                     <img className="card" src="./profileCard2.png" alt="profile cards"></img>
-                    <img className="card" src="./profileCard2.png" alt="profile cards"></img>
-                    <img className="card" src="./profileCard2.png" alt="profile cards"></img>
+                    <img className="card" src="./profileCard2.png" alt="profile cards"></img> */}
                 </div>
             </div>
             <div className="bottom-border left">
@@ -75,7 +75,7 @@ const Home = () => {
             </div>
             </div>
             <div className="text1">
-                <p className="header">Design Team 😎</p> 
+                <p className="header">Design Team</p> 
                 <p className="desc">Welcome to the realm of boundless creativity and visionary brilliance - our Design Team. With brushes dipped in imagination and pixels imbued with magic, they craft the tapestry of our club's visual identity.</p>
             </div>
             </div>
