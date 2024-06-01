71f194f800e0a18d47e8376ddec4abf7c3492132
Tridesh <tridesh2005@gmail.com>
2024-06-01 11:57:22 +0530

    update

--- pages/page.jsx
+++ pages/page.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./style.css";
-import Draggable from "react-draggable";
 
 const Home = () => {
     return (
-        <div>
+        <div>   
             <span className="rot-planet"><img className = "planet" src="./planet.png"></img></span>
             <img className = "missile" src="./missile.png"></img>
             <div className="text">Hello I'm Tridesh</div>
